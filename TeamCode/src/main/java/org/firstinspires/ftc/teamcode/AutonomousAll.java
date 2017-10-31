a2088f11c0ba091b3f8db3bcc650db3d6e1aca0f
cafrostrobotics <cafrostrobotics@gmail.com>
2017-10-31 16:48:31 -0400

    Initual checkin

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousAll.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode;
+import android.graphics.Color;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuMarkInstanceId;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+
+/**
+ * Created by cafrostrobotics on 10/31/2017.
+ */
+@Autonomous(name="Relic Recovery", group="2017")
+public class AutonomousAll extends OpMode {
+    //
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void loop() {
+    /*
+    * 1. Find Pictograph
+    * 2.Read Pictograph (c,l,r)
+    * 3. Move to jewels
+    * 4. Identify colours
+    * 5. Clear jewels.
+    * 6. Go to cryptobox
+    * 7. Find correct column
+    * 8. Place glyph
+    * 9. Get in cryptobox zone.
+    * Glyph 2
+    * 1. Find glyph
+    * 2. Get glyph
+    * 3. Identify colour
+    * 4.Determine target column
+    * 5. Goto cryptobox
+    * 5. Find corre3ct column
+    * 6. Place glyph
+    */
+    }
+
+    @Override
+    public void stop() {
+    }
+}
